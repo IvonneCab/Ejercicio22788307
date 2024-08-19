69e1ebe0bd056fb7f087e6113c948dd9eb771482
JACKAGU <JACKAGU@DESKTOP-6D37A3T>
2024-08-19 11:43:58 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio22788307/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿namespace Ejercicio22788307
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private readonly LocalDbService _dbService;
+        private int _editResultadoId;
+
+        public MainPage(LocalDbService dbService)
+        {
+            InitializeComponent();
+            _dbService = dbService;
+            Task.Run(async () => listview.ItemsSource = await _dbService.GetResultado());
+        }
+
+        private async void CalcularBtn_Clicked(object sender, EventArgs e)
+        {
+
+            string A = Entryprimernumero.Text;
+            string B = Entrysegundonumero.Text;
+
+            // Convertir los valores a números
+            if (!int.TryParse(A, out int a) || !int.TryParse(B, out int b))
+            {
+                await DisplayAlert("Error", "Los valores ingresados deben ser números enteros.", "Aceptar");
+                return;
+            }
+
+            // Calcular la suma
+            double Calcular = (Math.Pow(a + b, 2)) / 3;
+            // Asignar el resultado a la etiqueta
+            labelresultado.Text = Calcular.ToString();
+            if (_editResultadoId == 0)
+            {
+
+
+                await _dbService.Create(new Resultado
+                {
+                    A = Entryprimernumero.Text,
+                B = Entrysegundonumero.Text,
+                    Calcular = labelresultado.Text
+
+
+                });
+            }
+            else
+            {
+                await _dbService.Update(new Resultado
+                {
+                    Id = _editResultadoId,
+                    A = Entryprimernumero.Text,
+                   B = Entrysegundonumero.Text,
+                    Calcular = labelresultado.Text
+                });
+                _editResultadoId = 0;
+            }
+            Entryprimernumero.Text = string.Empty;
+            Entrysegundonumero.Text = string.Empty;
+            labelresultado.Text = string.Empty;
+
+            listview.ItemsSource = await _dbService.GetResultado();
+
+        }
+
+        private async void listview_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var resultado = (Resultado)e.Item;
+            var action = await DisplayActionSheet("Action", "Cancel", null, "Edit", "Delete");
+
+            switch (action)
+            {
+                case "Edit":
+                    _editResultadoId = resultado.Id;
+                    Entryprimernumero.Text = resultado.A;
+                    Entrysegundonumero.Text = resultado.B;
+                    labelresultado.Text = resultado.Calcular;
+                    break;
+
+                case "Delete":
+                    await _dbService.Delete(resultado);
+                    listview.ItemsSource = await _dbService.GetResultado();
+                    break;
+            }
+        }
+
+
+    }
+
+}
---
+++ Ejercicio22788307/Resultado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Ejercicio22788307
+{
+    [Table("Resultado")]
+   public class Resultado
+    {
+        [PrimaryKey, AutoIncrement]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("A")]
+
+        public string? A { get; set; }
+        [Column("B")]
+
+        public string? B { get; set; }
+
+        [Column("calcular")]
+        public string? Calcular { get; set; }
+    }
+}
